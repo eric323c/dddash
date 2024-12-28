e8a01504091013a07d9de9f31625938847efb47d
eric323c <71191021+eric323c@users.noreply.github.com>
2024-12-28 06:14:29 -0500

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -1,1 +1,15 @@
+// Open the Color Palette tool
+function openColorPalette() {
+    alert("Color Palette Tool coming soon!");
+}
+
+// Open the Typography Guide
+function openTypographyGuide() {
+    alert("Typography Guide coming soon!");
+}
+
+// Open the Shortcut Manager
+function openShortcutManager() {
+    alert("Shortcut Manager coming soon!");
+}
 
